d1360fe408ada198f6f94acc9781dee59778fb15
wytwolf <shn.career@gmail.com>
2017-05-26 19:06:41 +0530

    initiated main

--- app/src/main/java/com/example/samhan/app/sharelist/MainActivity.java
+++ app/src/main/java/com/example/samhan/app/sharelist/MainActivity.java
@@ -2,12 +2,54 @@ package com.example.samhan.app.sharelist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b_rock, b_paper, b_scissor;
+    ImageView iv_cpu, iv_me;
+
+    String playerChoice, cpuChoice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b_rock= (Button) findViewById(R.id.b_rock);
+        b_paper = (Button) findViewById(R.id.b_paper);
+        b_scissor = (Button) findViewById(R.id.b_scissor);
+
+        iv_cpu = (ImageView) findViewById(R.id.iv_cpu);
+        iv_me = (ImageView) findViewById(R.id.iv_me);
+
+        b_scissor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playerChoice = "scissor";
+                calculate();
+            }
+        });
+        b_paper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playerChoice = "paper";
+                calculate();
+            }
+        });
+        b_rock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playerChoice = "rock";
+                calculate();
+            }
+        });
+
     }
+
+    public void calculate(){
+
+    }
+
 }
